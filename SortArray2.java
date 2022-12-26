f54d7052749924ba25cafd46fcf9dbcf02661db9
Anna Istomina <a-ist33@mail.ru>
2022-12-26 11:20:18 +0500

    new sortArray

--- SortArray2.java
+++ SortArray2.java
@@ -1,9 +1,9 @@
 import java.util.Arrays;
 
-public class SortArray {
+public class SortArray2 {
 
     public static void main(String[] args) {
-        int[] arr = new int[100];
+        int[] arr = new int[100000000];
         for (int i = 0; i < arr.length; i++) {    // массив с рандомным набором чисел
             arr[i] = (int) (Math.random() * 100000000);
         }
@@ -15,30 +15,7 @@ public class SortArray {
             System.out.println("collectionSort = " + l + "ms");
             System.out.println("collectionSort = " + k + "nano");
         }
-
-        boolean isSorted = false;
-        {    // сортировка пузырьком
-            int buf;
-            while (!isSorted) {
-                isSorted = true;
-                for (int i = 0; i < arr.length - 1; i++) {
-                    if (arr[i] > arr[i + 1]) {
-                        isSorted = false;
-
-                        buf = arr[i];
-                        arr[i] = arr[i + 1];
-                        arr[i + 1] = buf;
-                    }
-                }
-                break;
-            }
-            long l = System.currentTimeMillis();
-            long k = System.nanoTime();
-            System.out.println("bufSort = " + l + "ms");
-            System.out.println("bufSort = " + k + "nano");
-
-        }
-            System.out.println();
+        System.out.println();
     }
 
 }
---
+++ Sortbuf.java
@@ -0,0 +1,31 @@
+public class Sortbuf {
+    public static void main(String[] args) {
+        int[] arr = new int[100000000];
+        for (int i = 0; i < arr.length; i++) {    // массив с рандомным набором чисел
+            arr[i] = (int) (Math.random() * 100000000);
+        }
+        boolean isSorted = false;
+        {    // сортировка пузырьком
+            int buf;
+            while (!isSorted) {
+                isSorted = true;
+                for (int i = 0; i < arr.length - 1; i++) {
+                    if (arr[i] > arr[i + 1]) {
+                        isSorted = false;
+
+                        buf = arr[i];
+                        arr[i] = arr[i + 1];
+                        arr[i + 1] = buf;
+                    }
+                }
+                break;
+            }
+            long l = System.currentTimeMillis();
+            long k = System.nanoTime();
+            System.out.println("bufSort = " + l + "ms");
+            System.out.println("bufSort = " + k + "nano");
+
+        }
+        System.out.println();
+    }
+}
